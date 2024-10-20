1afabdcfa573f2c48497b146c1a71a8a6fd8ad51
myungsu7929 <kmss7929@gmail.com>
2024-10-20 20:41:18 +0900

    final

--- config/hri_config.py
+++ config/hri_config.py
@@ -4,7 +4,7 @@ import os
 class HRIConfig():
     menu_wordset = ["진토닉", "마가리타", "마티니", "모히또", "모히토", "프렌치75", "프렌치 75", "섹스온더비치"]
     # recommand_wordset= ["추천"]
-    order_wordset = ["메뉴 준비해 드리겠습니다", "준비해 드리겠습니다", "준비해드리겠습니다", ""]
+    order_wordset = ["메뉴 준비해 드리겠습니다", "준비해 드리겠습니다", "준비해드리겠습니다"]
 
     positive_wordset = ['좋아', '맞아' ,'맞']
     negative_wordset = ['싫어', '아니야', '아니']
@@ -14,8 +14,6 @@ class HRIConfig():
 
     user_wav_path = "/home/sjlab3090/.temp_files/usr_voice.wav"
     user_mp3_path = "/home/sjlab3090/.temp_files/usr_voice.mp3"
-
-
 
 
 if __name__ == "__main__":
